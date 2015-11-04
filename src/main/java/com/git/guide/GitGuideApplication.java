82300e5789cdb34bfbbc302a62499b385a622883
venkatesha.chandru <venkatesha.chandru@rakuten.com>
2015-11-04 07:48:15 +0530

    Init Commit

---
+++ src/main/java/com/git/guide/GitGuideApplication.java
@@ -0,0 +1,12 @@
+package com.git.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitGuideApplication.class, args);
+    }
+}
