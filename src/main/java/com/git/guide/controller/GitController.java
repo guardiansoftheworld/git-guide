393ee75ef0cdba52255721eb779c84b0ae0dbc17
Prashanth L <Prashanth L@RIDOC-LPT010>
2015-11-26 18:46:21 +0900

    first Commit from prashanth

--- src/main/java/com/git/guide/controller/GitController.java
+++ src/main/java/com/git/guide/controller/GitController.java
@@ -1,5 +1,11 @@
 package com.git.guide.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class GitController {
+
+	public static void main(String arr[]) {
+		System.out.println(" changes done to local");
+	}
 
 }
