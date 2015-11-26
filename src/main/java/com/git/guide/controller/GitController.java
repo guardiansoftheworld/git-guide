e3cf2c517ce1e0ced3ea1afad25da5738ba71702
Prashanth L <Prashanth L@RIDOC-LPT010>
2015-11-26 19:15:48 +0900

    26-11-15. Time 7.15P.M

--- src/main/java/com/git/guide/controller/GitController.java
+++ src/main/java/com/git/guide/controller/GitController.java
@@ -5,7 +5,7 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 public class GitController {
 
 	public static void main(String arr[]) {
-		System.out.println(" changes done to local");
+		System.out.println("26-11-15");
 	}
 
 }
